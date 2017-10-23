f1cbfe83a3400dc515cd1e8f6010bf09f0e22d6d
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-10-23 18:24:14 +0800

    SortCommand: Add implementation

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,4 +1,47 @@
 package seedu.address.logic.commands;
 
-public class SortCommand {
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import javafx.collections.ObservableList;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.ReadOnlyPersonComparator;
+
+/**
+ * Sorts and displays the persons listing.
+ */
+public class SortCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "sort";
+    public static final String COMMAND_ALIAS = "st";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts and lists the Persons in the last person listing.\n"
+            + "Parameters: none"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SORT_SUCCESS = "Sorted list!";
+
+    private Predicate<ReadOnlyPerson> predicate = PREDICATE_SHOW_ALL_PERSONS;
+
+    public SortCommand() {
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+
+        ObservableList<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+        model.sortFilteredPersonList(lastShownList);
+
+        return new CommandResult(MESSAGE_SORT_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return true;
+    }
 }
