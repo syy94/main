7071e4c862348b80f35a6217f348e4252dedab1b
kengying <L.kengying@gmail.com>
2017-10-29 23:56:46 +0800

    Update with list group test case

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -34,7 +34,7 @@ public class ListCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_zeroTags_noPersonFound() {
+    public void execute_zeroKeyword_noPersonFound() {
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
         ListCommand command = prepareCommand(" ");
         assertCommandSuccess(command, expectedMessage, Collections.emptyList());
@@ -48,6 +48,13 @@ public class ListCommandTest {
     }
 
     @Test
+    public void execute_oneGroups_onePersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        ListCommand command = prepareCommand("Car");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(ELLE));
+    }
+
+    @Test
     public void execute_twoTags_sevenPersonFound() {
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 7);
         ListCommand command = prepareCommand("owesMoney friend");
@@ -55,7 +62,21 @@ public class ListCommandTest {
     }
 
     @Test
-    public void execute_wrongTags_zeroPersonFound() {
+    public void execute_twoGroups_sevenPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 7);
+        ListCommand command = prepareCommand("Car Savings");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+    }
+
+    @Test
+    public void execute_oneTagGroups_sevenPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        ListCommand command = prepareCommand("Car owesMoney");
+        assertCommandSuccess(command, expectedMessage, Arrays.asList(BENSON, ELLE));
+    }
+
+    @Test
+    public void execute_wrongKeyword_zeroPersonFound() {
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
         ListCommand command = prepareCommand("sdfsdf");
         assertCommandSuccess(command, expectedMessage, Collections.emptyList());
