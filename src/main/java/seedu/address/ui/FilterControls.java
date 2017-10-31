50bb1059fce7d7cd7045ad923147077c596e1cec
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-10-31 21:28:33 +0800

    FilterControls: Add ui for filter controls

---
+++ src/main/java/seedu/address/ui/FilterControls.java
@@ -0,0 +1,92 @@
+package seedu.address.ui;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.NewResultAvailableEvent;
+import seedu.address.logic.ListElementPointer;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.Logic;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.parser.Prefix;
+
+public class FilterControls extends UiPart<Region> {
+
+    private final Logic logic;
+    private final Logger logger = LogsCenter.getLogger(CommandBox.class);
+    private ListElementPointer historySnapshot;
+
+    private static final String FXML = "FilterControls.fxml";
+
+    @FXML
+    private ComboBox filterByDropdown;
+
+    @FXML
+    private Label dropdownLabel;
+
+    public FilterControls(Logic logic) {
+        super(FXML);
+        this.logic = logic;
+        setDropDown();
+    }
+
+    private void setDropDown() {
+        filterByDropdown.getItems().addAll(
+                "Name",
+                "Address",
+                "Phone",
+                "Email",
+                "Group"
+        );
+        filterByDropdown.getSelectionModel().selectFirst();
+        filterByDropdown.setOnAction((event) -> {
+            String selectedField = filterByDropdown.getSelectionModel().getSelectedItem().toString();
+            Prefix prefix = null;
+            switch (selectedField) {
+                case "Name": prefix = PREFIX_NAME;
+                    break;
+                case "Address": prefix = PREFIX_ADDRESS;
+                    break;
+                case "Phone": prefix = PREFIX_PHONE;
+                    break;
+                case "Email": prefix = PREFIX_EMAIL;
+                    break;
+                case "Group": prefix = PREFIX_GROUP;
+                    break;
+            }
+            try {
+                CommandResult commandResult = logic.execute("sort " + prefix);
+                initHistory();
+                historySnapshot.next();
+                logger.info("Result: " + commandResult.feedbackToUser);
+                raise(new NewResultAvailableEvent(commandResult.feedbackToUser));
+
+            } catch (CommandException | ParseException e) {
+                initHistory();
+                logger.info("Invalid sort");
+                raise(new NewResultAvailableEvent(e.getMessage()));
+            }
+        });
+    }
+
+    /**
+     * Initializes the history snapshot.
+     */
+    private void initHistory() {
+        historySnapshot = logic.getHistorySnapshot();
+        // add an empty string to represent the most-recent end of historySnapshot, to be shown to
+        // the user if she tries to navigate past the most-recent end of the historySnapshot.
+        historySnapshot.add("");
+    }
+}
