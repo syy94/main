0bb356d755b85c2d4ec2a226e1407cd1990e640c
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-10-31 21:48:02 +0800

    SortCommandTest: update use of Prefix in SortCommand

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -9,6 +12,7 @@ import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -25,39 +29,42 @@ public class SortCommandTest {
 
     @Test
     public void execute_sort_success() throws Exception {
-        SortCommand sortCommand = prepareCommand(model);
+        Prefix prefix = PREFIX_ADDRESS;
+        SortCommand sortCommand = prepareCommand(model, prefix);
         String expectedMessage = SortCommand.MESSAGE_SORT_SUCCESS;
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModel.sortFilteredPersonList(model.getFilteredPersonList());
+        expectedModel.sortFilteredPersonList(model.getFilteredPersonList(), prefix);
 
         assertCommandSuccess(sortCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_twoNamesList_success() throws Exception {
+    public void execute_twoPersonsList_success() throws Exception {
+        Prefix prefix = PREFIX_NAME;
         AddressBook ab = new AddressBookBuilder().withPerson(getTypicalPersons().get(1))
                 .withPerson(getTypicalPersons().get(0)).build();
         Model modelWithTwoPersons = new ModelManager(ab, new UserPrefs());
-        SortCommand sortCommand = prepareCommand(modelWithTwoPersons);
+        SortCommand sortCommand = prepareCommand(modelWithTwoPersons, prefix);
         String expectedMessage = SortCommand.MESSAGE_SORT_SUCCESS;
         Model expectedModel = new ModelManager(new AddressBook(modelWithTwoPersons.getAddressBook()), new UserPrefs());
-        expectedModel.sortFilteredPersonList(model.getFilteredPersonList());
+        expectedModel.sortFilteredPersonList(model.getFilteredPersonList(), prefix);
 
         assertCommandSuccess(sortCommand, modelWithTwoPersons, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_emptyList_throwsCommandException() {
+        Prefix prefix = PREFIX_EMAIL;
         Model emptyModel = new ModelManager(new AddressBook(), new UserPrefs());
         String expectedMessage = Messages.MESSAGE_EMPTY_PERSON_LIST;
-        assertCommandFailure(prepareCommand(emptyModel), model, expectedMessage);
+        assertCommandFailure(prepareCommand(emptyModel, prefix), model, expectedMessage);
     }
 
     /**
-     * Generates a new {@code SortCommand} with the Model given.
+     * Generates a new {@code SortCommand} with the Model and prefix given.
      */
-    private SortCommand prepareCommand(Model model) {
-        SortCommand sortCommand = new SortCommand();
+    private SortCommand prepareCommand(Model model, Prefix prefix) {
+        SortCommand sortCommand = new SortCommand(prefix);
         sortCommand.setData(model, new CommandHistory(), new UndoRedoStack());
         return sortCommand;
     }
