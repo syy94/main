32fcb8475dd85e08cde91323cb76e8a659e761bc
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-10-31 21:47:06 +0800

    SortCommandParserTest: Add tests for SortCommandParser

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SortCommand;
+
+public class SortCommandParserTest {
+
+    private SortCommandParser parser = new SortCommandParser();
+
+    @Test
+    public void parse_noArgs_returnSortCommand() {
+        assertParseSuccess(parser, "", new SortCommand());
+    }
+
+    @Test
+    public void parse_namePrefixArg_returnSortCommand() {
+        assertParseSuccess(parser, PREFIX_NAME.toString(), new SortCommand(PREFIX_NAME));
+    }
+
+    @Test
+    public void parse_validArg_returnSortCommand() {
+        assertParseSuccess(parser, PREFIX_ADDRESS.toString(), new SortCommand(PREFIX_ADDRESS));
+    }
+
+    @Test
+    public void parse_invalidArg_throwsParseException() {
+        assertParseFailure(parser, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_multipleArgs_throwsParseException() {
+        assertParseFailure(parser, PREFIX_EMAIL.toString() + PREFIX_GROUP.toString(),
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+}
