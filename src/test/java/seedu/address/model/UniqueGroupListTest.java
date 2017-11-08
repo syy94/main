093b49addf4bdeb6bb8442567fe829a3eb05bd89
kengying <L.kengying@gmail.com>
2017-11-08 22:33:58 +0800

    Update test case

---
+++ src/test/java/seedu/address/model/UniqueGroupListTest.java
@@ -0,0 +1,21 @@
+package seedu.address.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.model.group.UniqueGroupList;
+import seedu.address.model.tag.UniqueTagList;
+
+//@@author kengying
+public class UniqueGroupListTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
+        UniqueGroupList uniqueGroupList = new UniqueGroupList();
+        thrown.expect(UnsupportedOperationException.class);
+        uniqueGroupList.asObservableList().remove(0);
+    }
+}
